fa14a28ef4b3f6d9cfdb8c2fc542ca66d47f08ac
David wanjohi <davidwagura83@gmail.com>
2024-03-16 11:45:10 +0300

    modify main js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import './assets/tailwind.css'
+
+axios.defaults.baseURL = 'http://localhost:8000/api/';
+axios.defaults.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+
 
 createApp(App).mount('#app')
+
+App.use(router)
+
+App.mount('#app')
